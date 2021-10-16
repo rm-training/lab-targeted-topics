5400ecd1c7d1638724ddd30bc88f41c2a5dd8d0c
Ryan Morris <mr.morris@gmail.com>
2021-10-16 15:08:28 -0400

    Add Async labs

---
+++ public/async/demo/js/demo-1-a.js
@@ -0,0 +1,7 @@
+'use strict';
+
+console.log('hello from demo-1 A.js');
+
+// Ex: Selecting something in the page that has not necessarily been parsed yet...
+const selectedThing = document.getElementById('special-segment');
+console.log('Did I find "specia-segment"?', selectedThing);
--- spec/objects/calculator.spec.js
+++ spec/objects/calculator.spec.js
@@ -1,4 +1,4 @@
-import Calculator from "../../src/objects/calculator-solved.js";
+import Calculator from "../../public/objects/calculator-solved.js";
 
 describe("Targeted Topics - Objects - Exercise 2", function () {
 
